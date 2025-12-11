9159175d73057536b3d30cd8f7e23159edab39f7
nelson05122003 <nelsonkolas196@gmail.com>
2025-12-11 13:21:59 +0530

    fix: Removed

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button>Click Me</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
